d5b2aef1b43d5a72e5d847ccac28f4bcd9d407c0
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2024-11-17 18:34:44 -0700

    String methods and getters that can be used in both sets and lists
    Output:
    
    true
    Hite5h
    [H, tesh]
    Hi
    HITESH
    hitesh
    false
    true
    6

---
+++ StringOperations.dart
@@ -0,0 +1,13 @@
+void main()
+{
+  String a ="Hitesh";
+  print(a.contains("Hi"));
+  print(a.replaceAll('s','5'));
+  print(a.split('i'));
+  print(a.substring(0,2));
+  print(a.toUpperCase());
+  print(a.toLowerCase());
+  print(a.isEmpty);
+  print(a.isNotEmpty);
+  print(a.length);
+}
