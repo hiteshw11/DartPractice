d5c5b3450a2f53e53a3620f3f8d1bc504ffa489b
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2024-11-17 18:54:37 -0700

    Write a function that reverses a given string.
    Output
    
    hsetih

---
+++ ReverseString.dart
@@ -0,0 +1,10 @@
+// Write a function that reverses a given string.
+
+
+void main()
+{
+  String abc='hitesh';
+  List<String> def=abc.split('').toList();
+  List<String> name=def.reversed.toList();
+  print(name.join());
+}
